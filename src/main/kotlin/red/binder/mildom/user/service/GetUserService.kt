32243bb2a5650f0f1e624bfea5ef098aa12e0bbd
Akihiro Urushihara <a.urusihara@gmail.com>
2020-01-25 01:56:57 +0900

    remove limit.

--- src/main/kotlin/red/binder/mildom/user/service/GetUserService.kt
+++ src/main/kotlin/red/binder/mildom/user/service/GetUserService.kt
@@ -88,11 +88,6 @@ class GetUserService(
         while (!findEnds) {
             val latch = CountDownLatch(batch)
 
-            // 適度にやめる
-            if (10010000 < index.get()) {
-                break
-            }
-
             repeat(batch) {
                 threadPool.submit {
 
