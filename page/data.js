27205b99834ff0ddbf31a431a8a0be4c503e3998
(BOT) Akihiro Urushihara <bot.a.urusihara@gmail.com>
2023-07-13 09:16:28 +0000

    GetUserLiveData by CI (SHA e8ed51f925e9f11102eadf7b462a8067a6a8812c)

--- page/data.js
+++ page/data.js
@@ -1294,7 +1294,7 @@ var mildom_users = {
       "status": 10,
       "fans": 2986,
       "level": 99,
-      "viewer": 40
+      "viewer": 39
     },
     {
       "id": 10424908,
@@ -8962,7 +8962,7 @@ var mildom_users = {
       "status": 10,
       "fans": 268,
       "level": 68,
-      "viewer": 28
+      "viewer": 30
     },
     {
       "id": 10106088,
@@ -8989,7 +8989,7 @@ var mildom_users = {
       "status": 10,
       "fans": 266,
       "level": 90,
-      "viewer": 30
+      "viewer": 28
     },
     {
       "id": 10545167,
