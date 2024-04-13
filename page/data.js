58a98ad3f16cfa5b0fc1aa1b954e4a0c9983ab42
(BOT) Akihiro Urushihara <bot.a.urusihara@gmail.com>
2024-04-13 06:15:17 +0000

    GetUserLiveData by CI (SHA 326a89cd65c5477ef6e2630fc3e35f4deaa26b37)

--- page/data.js
+++ page/data.js
@@ -1906,7 +1906,7 @@ var mildom_users = {
       "status": 10,
       "fans": 1971,
       "level": 81,
-      "viewer": 15
+      "viewer": 18
     },
     {
       "id": 10499989,
@@ -5290,7 +5290,7 @@ var mildom_users = {
       "status": 10,
       "fans": 701,
       "level": 21,
-      "viewer": 97
+      "viewer": 80
     },
     {
       "id": 10784211,
